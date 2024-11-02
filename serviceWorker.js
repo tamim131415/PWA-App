8d4f3f7f6be6c3ff952f2096e735e0c4b4c69d9a
tamim131415 <abtamim131415@gmail.com>
2024-11-02 17:25:35 +0600

    Files added

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          'images/icon-192x192.webp',
+          'images/icon-512x512.webp'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
